4f79c441750818e5697e75e8bb12bdb271b1d416
sofiastepanova <159649742+sofiastepanova@users.noreply.github.com>
2024-11-02 12:49:59 +0400

    Update load.py

--- load.py
+++ load.py
@@ -4,6 +4,11 @@ from numpy import ndarray
 
 
 def l_image(image_path: str) -> ndarray:
+      """
+      Функция считывает изображение из файла
+      :param image_path: имя изображения
+      :return: изображение
+      """
     image = cv2.imread(image_path)
     if not exists(image_path):
         raise ValueError("Ошибка: Не удалось загрузить изображение.")
@@ -11,5 +16,10 @@ def l_image(image_path: str) -> ndarray:
 
 
 def h_w(image: ndarray) -> tuple:
+     """
+     Функция вывода высоты и ширины 
+     :param image: путь к изображению
+     :return: tuple
+     """
     height, width, channels = image.shape
     return width, height
